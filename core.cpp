8a5b80e36f4a6ca04c6117892e3f826e6acd1ff9
taHC81 <36913630+taHC81@users.noreply.github.com>
2019-03-26 13:48:31 +0100

    Update core.cpp

--- core.cpp
+++ core.cpp
@@ -13,6 +13,8 @@
 #include "Aeris.h"
 #elif defined(WEATHER_DARKSKY)
 #include "DarkSky.h"
+#elif defined(HA)
+#include "Homeauto.h"
 #else
 #include "Weather.h"
 #endif
@@ -238,6 +240,8 @@ static runStateClass::DurationAdjustments AdjustDurations(Schedule * sched)
 		Aeris w;
 #elif defined(WEATHER_DARKSKY)
 		DarkSky w;
+#elif defined(HA)
+		Homeauto w;
 #else
 		// this is a dummy provider which will just result in 100
 		Weather w;
